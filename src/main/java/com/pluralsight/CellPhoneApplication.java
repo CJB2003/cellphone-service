019e4b948403bf09bdebfee8c7156a19831b8ab8
chrisbunny <christopherjbunny@gmail.com>
2026-04-16 14:54:14 -0500

    Finished, may mess with it a bit

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,5 +1,54 @@
 package com.pluralsight;
+import java.util.*;
 
-public class CellPhoneApp {
-    
+public class CellPhoneApplication {
+    public static Scanner myScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        CellPhone serialNumber = new CellPhone();
+        CellPhone phoneModel = new CellPhone();
+        CellPhone phoneCarrier = new CellPhone();
+        CellPhone phoneNumber = new CellPhone();
+        CellPhone owner = new CellPhone();
+
+        //ask for user input with questions
+        System.out.print("What is the serial number? ");
+        int userSerial = myScanner.nextInt();
+        myScanner.nextLine();
+        System.out.print("What model is the phone? ");
+        String userModel = myScanner.nextLine();
+        System.out.print("Who is the carrier? ");
+        String userCarrier = myScanner.nextLine();
+        System.out.print("What is the phone number? ");
+        String userNumber = myScanner.nextLine();
+        System.out.print("Who is the owner of the phone? ");
+        String userName = myScanner.nextLine();
+
+
+        //set data with user input
+        serialNumber.setSerialNumber(userSerial);
+        phoneModel.setModel(userModel);
+        phoneCarrier.setCarrier(userCarrier);
+        phoneNumber.setPhoneNumber(userNumber);
+        owner.setOwner(userName);
+
+        //store getters in variables
+        int currentSN = serialNumber.getSerialNumber();
+        String currentPM = phoneModel.getModel();
+        String currentPC = phoneCarrier.getCarrier();
+        String currentNum = phoneNumber.getPhoneNumber();
+        String ownerName = owner.getOwner();
+
+        //print out string variables with getters stored
+        System.out.println(currentSN);
+        System.out.println(currentPM);
+        System.out.println(currentPC);
+        System.out.println(currentNum);
+        System.out.println(ownerName);
+
+
+
+
+
+    }
 }
